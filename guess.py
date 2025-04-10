e7fe6a0689c4251e22c7ba8a08e72636a8469e43
Yulia Novikova <yulia@MacBook-Pro.local>
2025-04-10 23:17:14 +0200

    Improve code structure and add README

--- guess.py
+++ guess.py
@@ -1,28 +1,36 @@
-# This is a guess the number game.
 import random
 
-print('Hello. What is your name?')
-name = input()
+def play_game():
+    print('Hello. What is your name?')
+    name = input()
 
-print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
-secretNumber = random.randint(1, 20)
+    print(f'Well, {name}, I am thinking of a number between 1 and 20.')
+    secretNumber = random.randint(1, 20)
 
-#print('DEBUG: Secret number is ' + str(secretNumber))
+    for guessesTaken in range(1, 7):
+        print('Take a guess.')
 
-for guessesTaken in range(1, 7):
-    print('Take a guess.')
-    guess = int(input())
+        try:
+            guess = int(input())
+        except ValueError:
+            print("That's not a number. Try again.")
+            continue
 
-    if guess < secretNumber:
-        print('Your guess is too low.')
-    elif guess > secretNumber:
-        print('Your guess is too high.')
+        if guess < secretNumber:
+            print('Your guess is too low.')
+        elif guess > secretNumber:
+            print('Your guess is too high.')
+        else:
+            print(f'Good job, {name}! You guessed my number in {guessesTaken} guesses!')
+            break
     else:
+        print(f'Nope. The number I was thinking of was {secretNumber}.')
+
+    print(f'You took {guessesTaken} guesses.')
+
+while True:
+    play_game()
+    play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+    if play_again not in ('yes', 'y'):
+        print("Thanks for playing! Goodbye.")
         break
-
-if guess == secretNumber:
-    print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken)  + ' guesses!') 
-else:
-    print('Nope. The number I was thinking of was ' + str(secretNumber))
-
-print('You took ' + str(guessesTaken) + ' guesses.')
